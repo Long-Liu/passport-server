8123f0ad805dbdc3bad7bab8033ee133b3816cdd
Long-Liu <1263601750@qq.com>
2018-01-10 11:30:15 +0800

    ApplicationConstants class can be convert to interface

--- src/main/java/org/infinity/passport/config/ApplicationConstants.java
+++ src/main/java/org/infinity/passport/config/ApplicationConstants.java
@@ -5,43 +5,41 @@ import java.util.Locale;
 /**
  * Application constants.
  */
-public final class ApplicationConstants {
+public interface ApplicationConstants {
 
-    public static final String   BASE_PACKAGE                   = "org.infinity.passport";
+    String BASE_PACKAGE = "org.infinity.passport";
 
     // Spring profile
-    public static final String   SPRING_PROFILES_ACTIVE         = "spring.profiles.active";
+    String SPRING_PROFILES_ACTIVE = "spring.profiles.active";
 
-    public static final String   SPRING_PROFILE_DEVELOPMENT     = "dev";
+    String SPRING_PROFILE_DEVELOPMENT = "dev";
 
-    public static final String   SPRING_PROFILE_TEST            = "test";
+    String SPRING_PROFILE_TEST = "test";
 
-    public static final String   SPRING_PROFILE_DEMO            = "demo";
+    String SPRING_PROFILE_DEMO = "demo";
 
-    public static final String   SPRING_PROFILE_PRODUCTION      = "prod";
+    String SPRING_PROFILE_PRODUCTION = "prod";
 
     // Spring profile used to disable swagger
-    public static final String   SPRING_PROFILE_NO_SWAGGER      = "no-swagger";
+    String SPRING_PROFILE_NO_SWAGGER = "no-swagger";
 
     // Spring profile used to disable AOP logging
-    public static final String   SPRING_PROFILE_NO_AOP_LOGGING  = "no-aop-logging";
+    String SPRING_PROFILE_NO_AOP_LOGGING = "no-aop-logging";
 
     // Spring profile used to enable service metrics
-    public static final String   SPRING_PROFILE_SERVICE_METRICS = "service-metrics";
+    String SPRING_PROFILE_SERVICE_METRICS = "service-metrics";
 
     // Spring profile used to tracing request with Spring Cloud Zipkin
-    public static final String   SPRING_PROFILE_ZIPKIN          = "zipkin";
+    String SPRING_PROFILE_ZIPKIN = "zipkin";
 
-    public static final String[] AVAILABLE_PROFILES             = new String[] { SPRING_PROFILE_DEVELOPMENT,
+    String[] AVAILABLE_PROFILES = new String[]{SPRING_PROFILE_DEVELOPMENT,
             SPRING_PROFILE_TEST, SPRING_PROFILE_DEMO, SPRING_PROFILE_PRODUCTION, SPRING_PROFILE_NO_SWAGGER,
-            SPRING_PROFILE_NO_AOP_LOGGING, SPRING_PROFILE_ZIPKIN, SPRING_PROFILE_SERVICE_METRICS };
+            SPRING_PROFILE_NO_AOP_LOGGING, SPRING_PROFILE_ZIPKIN, SPRING_PROFILE_SERVICE_METRICS};
 
-    public static final String   SYSTEM_ACCOUNT                 = "system";
+    String SYSTEM_ACCOUNT = "system";
 
-    public static final String   SCHEDULE_LOG_PATTERN           = "########################Schedule executed: {}########################";
+    String SCHEDULE_LOG_PATTERN = "########################Schedule executed: {}########################";
 
-    public static final Locale   SYSTEM_LOCALE                  = Locale.SIMPLIFIED_CHINESE;
+    Locale SYSTEM_LOCALE = Locale.SIMPLIFIED_CHINESE;
 
-    private ApplicationConstants() {
-    }
 }
