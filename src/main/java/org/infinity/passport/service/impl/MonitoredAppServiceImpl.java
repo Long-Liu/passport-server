f9c18576abfceffa73608213b15cebc81c09642b
Long-Liu <1263601750@qq.com>
2018-01-11 10:34:50 +0800

    biz

--- src/main/java/org/infinity/passport/service/impl/MonitoredAppServiceImpl.java
+++ src/main/java/org/infinity/passport/service/impl/MonitoredAppServiceImpl.java
@@ -5,6 +5,8 @@ import org.infinity.passport.domain.HealthState;
 import org.infinity.passport.domain.MonitoredApp;
 import org.infinity.passport.domain.Node;
 import org.infinity.passport.service.MonitoredAppService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.ResponseEntity;
@@ -17,6 +19,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 @Service
 public class MonitoredAppServiceImpl implements MonitoredAppService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MonitoredAppServiceImpl.class);
+
     private final MongoTemplate mongoTemplate;
 
     private final RestTemplate restTemplate;
@@ -30,11 +34,11 @@ public class MonitoredAppServiceImpl implements MonitoredAppService {
     @Override
     public List<MonitoredApp> findAll() {
         List<MonitoredApp> all = mongoTemplate.findAll(MonitoredApp.class);
-        all.parallelStream().forEach(e -> {
-        AtomicInteger errorSize = new AtomicInteger(0);
-            e.getNodes().parallelStream().forEach(o -> {
+        all.forEach(e -> {
+            AtomicInteger errorSize = new AtomicInteger(0);
+            e.getNodes().forEach(o -> {
                 try {
-                    String statusName = getNodeStatus(o).equals("up") ? "健康" : "出错";
+                    String statusName = getNodeStatus(o).substring(1,14).equals("\"status\":\"UP\"") ? "健康" : "出错";
                     String statusColor = statusName.equals("健康") ? "蓝色" : "红色";
                     HealthState healthState = new HealthState(statusName, statusColor);
                     o.setHealthState(healthState);
@@ -45,20 +49,21 @@ public class MonitoredAppServiceImpl implements MonitoredAppService {
                     errorSize.getAndIncrement();
                 }
             });
-            if (all.size() >>> 2 < errorSize.get()) {
+            int i = errorSize.get();
+            if (all.size() >>> 1 < i) {
                 e.setHealthState(new HealthState("出错", "红色"));
-            } else if (all.size() >>> 2 >= errorSize.get() && errorSize.get() >= 1) {
+            } else if (all.size() >>> 1 >= i && i >= 1) {
                 e.setHealthState(new HealthState("警告", "橙色"));
             } else {
-                e.setHealthState(new HealthState("正常", "蓝色"));
+                e.setHealthState(new HealthState("健康", "蓝色"));
             }
         });
         return all;
     }
 
-    private String getNodeStatus(Node node) throws Exception {
-        ResponseEntity<Map> entity = restTemplate.getForEntity("http://" + node.getServerAddress()
-                + node.getPort() + node.getHealthContextPath(), Map.class);
-        return ((String) entity.getBody().get("status"));
+    private String getNodeStatus(Node node) {
+        ResponseEntity<String> entity = restTemplate.getForEntity("http://" + node.getServerAddress() + ":"
+                + node.getPort() + node.getHealthContextPath(), String.class);
+        return entity.getBody();
     }
 }
